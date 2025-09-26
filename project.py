7a0afceac5d0a482b0f3352f27746cc5016a732f
Girindrashinie Luxshumy <155840065+buttercup0209@users.noreply.github.com>
2025-09-27 02:29:59 +0800

    Creating basic window setup

--- project.py
+++ project.py
@@ -5,3 +5,14 @@ import os
 import uuid
 from datetime import datetime
 import shutil
+
+#Main app window
+root = tk.Tk()
+root.title("Lost & Found System")
+root.geometry("400x300")
+
+#Label
+title_label = tk.Label(root, text="Lost & Found Management System", font=("Arial", 16, "bold"))
+title_label.pack(pady=50)
+
+root.mainloop()
