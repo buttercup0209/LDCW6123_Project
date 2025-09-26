d07fe904c9ec5ba6162fd27e4319ad177bc7de1f
Girindrashinie Luxshumy <155840065+buttercup0209@users.noreply.github.com>
2025-09-27 02:33:23 +0800

    Addng JSON data functions

--- project.py
+++ project.py
@@ -6,6 +6,20 @@ import uuid
 from datetime import datetime
 import shutil
 
+DATA_FILE = "lost_found.json"
+
+# Load existing data
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return []
+
+# Save data
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
 #Main app window
 root = tk.Tk()
 root.title("Lost & Found System")
